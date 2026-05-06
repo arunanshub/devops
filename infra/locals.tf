719d20ce4a4403f5462fd0742366537609d8b5cd
Arunanshu Biswas <mydellpc07@gmail.com>
2026-05-06 19:20:35 +0530

    update locals

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,6 +1,6 @@
 locals {
   cluster_name = "hetzner-k3s"
-  k3s_version  = "v1.32.3+k3s1"
+  k3s_version  = "v1.35.4+k3s1"
   # network CIDR for private networking. Must be large enough to accommodate all nodes and services.
   network_cidr     = "10.0.0.0/16"
   subnet_cidr      = cidrsubnet(local.network_cidr, 8, 0)
